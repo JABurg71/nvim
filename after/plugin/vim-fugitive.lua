37080a59283b75d9eccdfa93482b4e7b616f28b4
JABurg71 <96436672+JABurg71@users.noreply.github.com>
2024-02-12 21:17:23 -0600

    Added fancy graph shortcut to fugitive
    Prior to this change,
    No visuals for git
    
    This change
    Added visual for git commits

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,3 +1,4 @@
 vim.keymap.set('n', '<leader>G', ":vertical Git<CR>")
 vim.keymap.set('n', '<leader>gla', ":vertical Git log<CR>")
 vim.keymap.set('n', '<leader>glo', ":vertical Git log --oneline<CR>")
+vim.keymap.set('n', '<leader>glf', ":vertical Git log --all --decorate --oneline --graph<CR>")
